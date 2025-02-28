afbfd49ad7a065e40dd8ad9cced77793a5f2c9ec
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-01 03:28:43 +0400

    fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 function fibonacci(count, n1 = 0, n2 = 1) {
 	if (count == 0) return "";
-	return n1 + n2 + "\n" + fibonacci(count - 1, n2, n1 + n2);
+	return n1 + ", " + fibonacci(count - 1, n2, n1 + n2);
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(8));
