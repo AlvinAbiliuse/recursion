e389751017eb938cdfa2574f4c44876b6881f21b
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-01 10:19:02 +0400

    bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+let arr = [
+	1, 142, 153, 15, 95, 4, 421, 124, 65, 86, 45, 7436, 53, 87, 24, 423, 754, 87,
+	1,
+];
+
+let arr2 = [1, 5, 6, 8, 2, 4, 3, 9, 0, 7];
+
+console.log("\n\n");
+console.log(arr);
+console.log(bubbleSort(arr));
+console.log("\n\n");
+console.log(arr2);
+console.log(bubbleSort(arr2));
