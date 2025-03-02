1c7cd2c68677b6bc2caf40082719b820e84721e6
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-03 01:50:12 +0400

    fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,12 @@
-function fib(c, n1 = 0, n2 = 1) {
-	if (c == 0) return "";
-	return (c == 1 ? n1 : `${n1}, `) + fib(c - 1, n2, n1 + n2);
+function fib(n) {
+	let count = 0;
+	let n1 = 0;
+	let n2 = 1;
 }
-console.log(fib(21));
+
+function fibRec(c, n1 = 0, n2 = 1) {
+	if (c == 0) return "";
+	return (c == 1 ? n1 : `${n1}, `) + fibRec(c - 1, n2, n1 + n2);
+}
+
+console.log(fibRec(21));
