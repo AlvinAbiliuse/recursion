2861810580a08cf665f4515c3927dd55e62603cf
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-04 02:13:40 +0400

    bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,28 +3,26 @@ function merge(left, right) {
 	let j = 0;
 	let k = 0;
 	let mergedArr = [];
-	while (left.length > 0 && right.length > 0) {
-		console.log(left[0], right[0]);
-		if (left[0] < right[0]) {
-			mergeArr.push(left.shift());
-		} else {
-			mergeArr.push(right.shift());
+	while (c !== left.length || j !== right.length) {
+		if (c == left.length) {
+			mergedArr.push(right[j++]);
+		} else if (j == right.length) {
+			mergedArr.push(left[c++]);
+		} else if (left[c] < right[j]) {
+			mergedArr.push(left[c++]);
+		} else if (right[j] < left[c]) {
+			mergedArr.push(right[j++]);
 		}
-	}
-	if (left.length !== 0) {
-		mergeArr.push(left.shift());
-	} else if (right.length !== 0) {
-		mergeArr.push(right.shift());
 	}
 	return mergedArr;
 }
 
 function bubbleSort(arr) {
-	if ((arr.length = 1)) return arr;
+	if (arr.length === 1) return arr;
 	let halfway = Math.floor(arr.length / 2);
 
-	let lh = arr.slice(0, halfway);
-	let rh = arr.slice(halfway, arr.length);
+	let lh = bubbleSort(arr.slice(0, halfway));
+	let rh = bubbleSort(arr.slice(halfway, arr.length));
 	return merge(lh, rh);
 }
 
@@ -37,7 +35,9 @@ let sortArr2 = [4, 5, 6, 8, 2, 1, 3, 9, 0, 7];
 
 console.log("\n\n");
 // console.log(sortArr);
-console.log(bubbleSort(sortArr));
+// console.log(
+//bubbleSort(sortArr);
+// );
 console.log("\n\n");
-// console.log(sortArr2);
+console.log(sortArr2);
 console.log(bubbleSort(sortArr2));
