1fd01af545f68933ad2c676b9bb0f95f013b36e6
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-04 01:45:09 +0400

    bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,13 +1,25 @@
-let arr = [
+function merge(left, right) {}
+
+function bubbleSort(arr, arr2 = []) {
+	if ((arr.length = 1)) return arr;
+	let halfway = Math.floor(arr.length / 2);
+
+	let lh = bubbleSort(arr.slice(0, halfway));
+	let rh = bubbleSort(arr.slice(halfway, arr.length));
+}
+
+let sortArr = [
 	1, 142, 153, 15, 95, 4, 421, 124, 65, 86, 45, 7436, 53, 87, 24, 423, 754, 87,
 	1,
 ];
 
-let arr2 = [1, 5, 6, 8, 2, 4, 3, 9, 0, 7];
+let sortArr2 = [1, 5, 6, 8, 2, 4, 3, 9, 0, 7];
 
+/*
 console.log("\n\n");
-console.log(arr);
-console.log(bubbleSort(arr));
+console.log(sortArr);
+console.log(bubbleSort(sortArr));
 console.log("\n\n");
-console.log(arr2);
-console.log(bubbleSort(arr2));
+console.log(sortArr2);
+console.log(bubbleSort(sortArr2));
+*/
