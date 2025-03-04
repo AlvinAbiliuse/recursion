7aac3a09dd9a8db6b025f0eb5af3c34cefbda16d
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-05 02:30:44 +0400

    fibonacci.js fixed typo

--- fibonacci.js
+++ fibonacci.js
@@ -17,14 +17,6 @@ function fib(n) {
 }
 
 function fibRec(c, n1 = 0, n2 = 1) {
-	/*
-	let arr = [];
-
-	if (c == 0) return arr;
-	arr.concat(n1);
-	fibRec(c - 1, n2, n2 + n1);
-	return arr.concat(n1).concat(fibRec(c - 1, n2, n1 + n2));
-	*/
 	if (c == 0) return "";
 	return [n1, ...fibRec(c - 1, n2, n1 + n2)];
 	/* comma seperate string version: 
@@ -33,18 +25,18 @@ function fibRec(c, n1 = 0, n2 = 1) {
 }
 
 console.log("\nFibonacci\n");
-console.log("Iterative Version:");
+console.log("\nIterative Version:\n");
 let iterativeTime = new Date().getTime();
 console.log(fib(20));
 console.log(
-	"Elapsed TIme (seconds): ",
+	"\nElapsed Time (seconds): ",
 	(new Date().getTime() - iterativeTime) / 1000
 );
-console.log("\nRecursive Version:");
-iterativeTime = new Date().getTime();
+console.log("\n\nRecursive Version:\n");
+let recursiveTime = new Date().getTime();
 console.log(fibRec(20));
 console.log(
-	"Elapsed Time (seconds): ",
-	(new Date().getTime() - iterativeTime) / 1000
+	"\nElapsed Time (seconds): ",
+	(new Date().getTime() - recursiveTime) / 1000
 );
 console.log("\n");
