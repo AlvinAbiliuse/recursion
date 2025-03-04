854f34c6ac4e3baa309b289a28be37a6b58871e7
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-05 02:25:30 +0400

    mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,27 +1,47 @@
 function merge(left, right) {
 	let c = 0;
 	let j = 0;
+	let k = left.length + right.length;
 	let mergedArr = [];
-	while (c !== left.length || j !== right.length) {
-		if (c == left.length) {
-			mergedArr.push(right[j++]);
-		} else if (j == right.length) {
-			mergedArr.push(left[c++]);
-		} else if (left[c] < right[j]) {
+	for (let i = 0; i < k; i++) {
+		if (left.lenght === 0) {
+			mergedArr.push(...right);
+		} else if (right.length === 0) {
+			mergedArr.push(...left);
+		} else if (left[0] < right[0]) {
+			mergedArr.push(left.shift());
+		} else if (right[0] < left[0]) {
+			mergedArr.push(right.shift());
+		}
+	}
+	mergedArr.push(...left);
+	mergedArr.push(...right);
+
+	/*
+	while (c !== left.length && j !== right.length) {
+		console.log(c, j);
+		console.log(left, right);
+		if (left[c] < right[j]) {
 			mergedArr.push(left[c++]);
 		} else if (right[j] < left[c]) {
 			mergedArr.push(right[j++]);
 		}
 	}
+	if (c == left.length) {
+		mergedArr.push(right[j++]);
+	} else if (j == right.length) {
+		mergedArr.push(left[c++]);
+	}
+	*/
 	return mergedArr;
 }
 
 function mergeSort(arr) {
-	if (arr.length < 2) return arr;
+	if (arr.length === 1) return arr;
 	let halfway = Math.floor(arr.length / 2);
 
 	let lh = mergeSort(arr.slice(0, halfway));
-	let rh = mergeSort(arr.slice(halfway + 1, arr.length));
+	let rh = mergeSort(arr.slice(halfway, arr.length));
 	return merge(lh, rh);
 }
 
@@ -31,13 +51,29 @@ function mergeSort(arr) {
 ]
 
 let sortArr2 = [4, 5, 6, 8, 2, 1, 3, 9, 0, 7];
-
 */
 
 let sortArr = [3, 2, 1, 13, 8, 5, 0, 1];
 
 let sortArr2 = [105, 79, 100, 110];
+/*
+let m1 = merge([4, 5, 6, 8, 2], [1, 3, 9, 0, 7]);
+let m2 = merge([105, 79], [100, 110]);
+let m1Second = merge(
+	m1.slice(0, Math.floor(m1.length / 2)),
+	m1.slice(Math.floor(m1.length / 2), m1.length)
+);
+let m2Second = merge(
+	m2.slice(0, Math.floor(m2.length / 2)),
+	m2.slice(Math.floor(m2.length / 2), m2.length)
+);
 
+console.log(m1);
+console.log(m1Second);
+console.log(m2);
+console.log(m2Second);
+
+*/
 console.log("\n\n");
 console.log(sortArr);
 console.log(mergeSort(sortArr));
