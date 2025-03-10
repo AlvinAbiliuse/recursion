4a1872663c572bb6e79c826d0b020cae67f79c9f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-10 06:07:04 +0400

    mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -16,26 +16,13 @@ function merge(left, right) {
 	return mergedArr;
 }
 
-function merger(left, right) {
-	let c = 0;
-	let j = 0;
-	while (left.length != c || right.length != j) {
-		if (left[c] < right[j]) {
-			j++;
-		}
-	}
-}
-
 function mergeSort(arr) {
 	if (arr.length === 1) return arr;
 	let halfway = Math.floor(arr.length / 2);
 
 	let lh = mergeSort(arr.slice(0, halfway));
 	let rh = mergeSort(arr.slice(halfway, arr.length));
-	let merged = merge(lh, rh);
-	console.log(lh, rh);
-	console.log(merged);
-	return merged;
+	return merge(lh, rh);
 }
 
 /* let sortArr = [
@@ -49,13 +36,10 @@ let sortArr2 = [4, 5, 6, 8, 2, 1, 3, 9, 0, 7];
 let sortArr = [3, 2, 1, 13, 8, 5, 0, 1];
 
 let sortArr2 = [105, 79, 100, 110];
-/*
+
 console.log("\n\n");
 console.log(sortArr);
 console.log(mergeSort(sortArr));
 console.log("\n\n");
 console.log(sortArr2);
 console.log(mergeSort(sortArr2));
-*/
-
-console.log(merge([1, 2, 3, 13], [0, 1, 5, 8]));
