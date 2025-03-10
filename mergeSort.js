8a889b80ac77e72f1009169274b2ca9d8cd9c0de
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-10 06:22:15 +0400

    mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -22,22 +22,36 @@ function mergeSort(arr) {
 	let rh = mergeSort(arr.slice(halfway, arr.length));
 	return merge(lh, rh);
 }
-
-/* let sortArr = [
+let sortArr = [
 	1, 142, 153, 15, 95, 4, 421, 124, 65, 86, 45, 7436, 53, 87, 24, 423, 754, 87,
 	1,
-]
+];
 
 let sortArr2 = [4, 5, 6, 8, 2, 1, 3, 9, 0, 7];
-*/
 
+/* 
 let sortArr = [3, 2, 1, 13, 8, 5, 0, 1];
 
 let sortArr2 = [105, 79, 100, 110];
 
+*/
+
+let startTime, endTime;
+console.log("\n\nMerge Sort");
 console.log("\n\n");
+console.log("Unsorted: ");
 console.log(sortArr);
+console.log("\nSorted: ");
+startTime = new Date();
 console.log(mergeSort(sortArr));
+endTime = new Date();
+console.log(`\nTime Elapsed: ${endTime.getTime() - startTime.getTime()} ms`);
 console.log("\n\n");
+console.log("Unsorted: ");
 console.log(sortArr2);
+console.log("\nSorted: ");
+startTime = new Date();
 console.log(mergeSort(sortArr2));
+endTime = new Date();
+console.log(`\nTime Elapsed: ${endTime.getTime() - startTime.getTime()} ms`);
+console.log("\n\n");
